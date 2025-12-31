e21fce015b368817d9c420fdb2b00d4be00d046e
tob-david <gerardus.david@tob-ins.com>
2025-12-31 13:33:19 +0700

    feat: add handlers

---
+++ src/handlers/customersHandler.ts
@@ -0,0 +1,18 @@
+import * as restate from "@restatedev/restate-sdk";
+import { findAllAccounts, findCustomerById } from "../database/queries";
+
+export const customerService = restate.service({
+  name: "CustomerService",
+  handlers: {
+    getCustomers: async (ctx: restate.Context) => {
+      const customers = await ctx.run("getCustomers", async () => {
+        console.log("Fetching customers...");
+        return await findAllAccounts();
+      });
+
+      return customers;
+    },
+  },
+});
+
+export type CustomerService = typeof customerService;
