b32f299413d6c816d8a751c850af80657299326f
tob-david <gerardus.david@tob-ins.com>
2025-12-31 13:36:33 +0700

    feat: update service and workflow

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import * as restate from "@restatedev/restate-sdk";
+import { soaProcessHandler } from "./handlers/soaProcessHandler";
+import { customerService } from "./handlers/customersHandler";
 import { soaWorkflow } from "./workflows/soaWorkflow";
+import { soaProcessingWorkflow } from "./workflows/soaProcessingWorkflow";
+import { batchService } from "./handlers/batchHandler";
 
 restate.serve({
-  services: [soaWorkflow],
+  services: [
+    soaWorkflow,
+    soaProcessingWorkflow,
+    customerService,
+    batchService,
+    soaProcessHandler,
+  ],
   port: 9080,
 });
